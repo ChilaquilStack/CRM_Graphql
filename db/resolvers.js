fbec4aedf488607b34917014481a1a1b6d884f49
Edgar_Villegas@epam.com <Edgar_Villegas@epam.com>
2022-01-11 16:20:17 -0600

    El contexto

--- db/resolvers.js
+++ db/resolvers.js
@@ -3,7 +3,10 @@ const cursos = require('./cursos')
 const resolvers = {
     
     Query:{
-        getCursos: (_ , {input}, ctx, info) => cursos.filter(curso => curso.tecnologia === input.tecnologia),
+        getCursos: (_ , {input}, ctx, info) => {
+            console.log(ctx)
+            return cursos.filter(curso => curso.tecnologia === input.tecnologia)
+        },
         getTecnologia: () => cursos
     }
 
--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const resolvers = require('./db/resolvers')
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: () => ({
+        usuario: {
+            id: 1
+        }
+    })
 })
 
 server.listen().then(({url}) => console.log(`Servidor listo en la URL ${url}`))

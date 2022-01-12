a2d976dfa931a2c0072427b5d2577ee409a5de56
Edgar_Villegas@epam.com <Edgar_Villegas@epam.com>
2022-01-11 18:28:10 -0600

    Cifrando la contraseña

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,12 +1,15 @@
+const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/Usuario.model');
 
 const resolvers = {
     
     Mutation: {
         createUsuario: async (_, {input}) => {
-            const { email } = input
+            const { email, password } = input
             const usuarioExists = await Usuario.findOne({email})
             if(usuarioExists) throw new Error('El usuario ya esta registrado')
+            const salt = await bcryptjs.genSalt(10)
+            input.password = await bcryptjs.hash(password, salt)
             try {
                 const usuario = new Usuario(input)
                 usuario.save()
